a144b091a603057860bc406129d6da7a2416c5c6
GunPKate <gunPK>
2023-08-20 22:07:19 +0700

    test1 test2 done

--- test1GetScore.ts
+++ test1GetScore.ts
@@ -62,31 +62,3 @@ getHandScore("SK DJ DA");
 getHandScore("S8 D8 C8"); 
 getHandScore("SA DA CA"); 
 
-let a:string = "9:00";
-let b:string = "18:01";
-// let c:string = "12:00";
-let c:string = "17:30";
-
-function getAngleClock(hh_mm: string){
-  let tempTime = hh_mm.split(":")
-  let h :number = +tempTime[0];
-  h >= 12?h=  (h - 12) : h;
-
-  let m :number = +tempTime[1];
- 
-  //  console.log(h,m);
-
-  let angleM: number = (m/60)*360;
-  let angleH: number = ((h + (m/60))/12*360);
-  // console.log(angleH,angleM);
-  
-  let diff:number  = (angleM - angleH);
-  diff< 0? diff *=-1: diff;
-  diff > 180? diff -= 180: diff; //convert to short angle
-  console.log("Test#1 =",diff);
-  return diff;
-}
-
-getAngleClock(a);
-getAngleClock(b);
-getAngleClock(c);
